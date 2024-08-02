1c6eb9932ba8eb3771726480936278af26fc835a
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-08-02 11:43:23 -0300

    add: observatorio do turismo dump_url and dbt pipelines

---
+++ pipelines/observatorio_turismo/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from pipelines.observatorio_turismo.dump_url.flows import *  # noqa
+from pipelines.observatorio_turismo.dbt.flows import *  # noqa
---
+++ pipelines/observatorio_turismo/dbt/schedules.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for the database dump pipeline
+"""
+
+from datetime import datetime, timedelta
+
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+import pytz
+
+from pipelines.constants import constants
+from prefeitura_rio.pipelines_utils.io import untuple_clocks as untuple
+
+#####################################
+#
+# SETUR Seeketing Schedules
+#
+#####################################
+
+setur_seeketing_tables = {
+    "aeroportos": "aeroportos",
+    "pontos_turisticos": "pontos_turisticos",
+    "caged": "caged",
+    "empregos": "empregos",
+    "iss_turistico": "iss_turistico",
+    "museus": "museus",
+    "rede_hoteleira_ocupacao_eventos": "rede_hoteleira_ocupacao_eventos",
+    "rede_hoteleira_ocupacao_geral": "rede_hoteleira_ocupacao_geral",
+    "rodoviarias": "rodoviarias",
+}
+
+setur_seeketing_clocks = [
+    IntervalClock(
+        interval=timedelta(days=1),
+        start_date=datetime(
+            2023, 3, 28, 17, 30, tzinfo=pytz.timezone("America/Sao_Paulo")
+        )
+        + timedelta(minutes=1 * count),
+        labels=[
+            constants.RJ_SETUR_AGENT_LABEL.value,
+        ],
+        parameter_defaults={
+            "dataset_id": "turismo_fluxo_visitantes",
+            "table_id": table_id,
+            "mode": "prod",
+        },
+    )
+    for count, (_, table_id) in enumerate(setur_seeketing_tables.items())
+]
+
+# Add parameter materialize_to_datario as True only for the table rede_hoteleira_ocupacao_eventos
+setur_seeketing_clocks[6].parameter_defaults["materialize_to_datario"] = True
+
+setur_seeketing_daily_update_schedule = Schedule(clocks=untuple(setur_seeketing_clocks))
